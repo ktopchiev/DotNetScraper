3cef615f6488e87541e4e87a7ede57dd56836def
ktopchiev <karlostopchiev@gmail.com>
2021-01-10 16:08:42 +0200

    Cannot access the children of details container

--- DotNetScraper/Program.cs
+++ DotNetScraper/Program.cs
@@ -2,7 +2,6 @@
 using System.Net;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Web;
